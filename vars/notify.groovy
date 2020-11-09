8c994d90f3e27829e819c5dd15c7bedca413175a
madhusakthivel <70783458+madhusakthivel@users.noreply.github.com>
2020-11-09 16:10:41 -0500

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -5,7 +5,10 @@ def call(Map config=[:]) {
         echo config.message
     } else {
         echo Constants.EMAIL_MESSAGE
-        echo config.message
-		mail bcc: '', body: '''Hello Madhu,''', cc: '', from: '', replyTo: '', subject: 'Jenkins Shared Library Notification', to: 'madhu.spmm@gmail.com'
+        /*echo config.message
+		mail bcc: '', body: '''Hello Madhu,''', cc: '', from: '', replyTo: '', subject: 'Jenkins Shared Library Notification', to: 'madhu.spmm@gmail.com'*/
+	    emailext attachLog: true, body: "Hello User, Please find the build status and attaached log.\n ${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} config.message \n More info at: ${env.BUILD_URL}", 
+				recipientProviders: [buildUser()], 
+				subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}", to: 'madhu.spmm@gmail.com'
     }
 }
