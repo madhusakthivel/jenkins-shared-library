0e4791b3b841e76707b04e6d1212fc7c338c4d81
madhusakthivel <70783458+madhusakthivel@users.noreply.github.com>
2020-11-09 17:08:24 -0500

    Update notify.groovy

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -3,12 +3,15 @@ def call(Map config=[:]) {
     if (config.type == "slack") {
         echo Constants.SLACK_MESSAGE
         echo config.message
+	    slackSend channel: '#jenkins', message: config.message
     } else {
         echo Constants.EMAIL_MESSAGE
+	emailext body: config.message, subject: 'Build Status', to: 'madhu.spmm@gmail.com'
         /*echo config.message
 		mail bcc: '', body: '''Hello Madhu,''', cc: '', from: '', replyTo: '', subject: 'Jenkins Shared Library Notification', to: 'madhu.spmm@gmail.com'*/
-	    emailext attachLog: true, body: "Hello User, Please find the build status and attaached log.\n ${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} config.message \n More info at: ${env.BUILD_URL}", 
+	   ?/* emailext attachLog: true, body: "Hello User, Please find the build status and attaached log.\n ${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER} config.message \n More info at: ${env.BUILD_URL}", 
 				recipientProviders: [buildUser()], 
-				subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}", to: 'madhu.spmm@gmail.com'
+				subject: "Jenkins Build ${currentBuild.currentResult}: Job ${env.JOB_NAME}", to: 'madhu.spmm@gmail.com'*/
+		   
     }
 }
